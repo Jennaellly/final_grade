65afa0963b9b4ae11a50ada3afc9e02f96c522a6
￴￴ <>
2024-06-07 15:17:59 +0700

    up

---
+++ bin/final_grade.dart
@@ -0,0 +1,5 @@
+import 'package:final_grade/final_grade.dart' as final_grade;
+
+void main(List<String> arguments) {
+  print('Hello world: ${final_grade.calculate()}!');
+}
---
+++ test/final_grade_test.dart
@@ -0,0 +1,8 @@
+import 'package:final_grade/final_grade.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
